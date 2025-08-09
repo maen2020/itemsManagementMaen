6d35795dcfe5452a130dba01073ce1ea6cde3727
maen2020 <maen210212@gmail.com>
2025-08-09 13:48:50 -0600

    Start of the project.

---
+++ src/main/java/com/maen/ItemStoreAPI/ItemStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.maen.ItemStoreAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maen/ItemStoreAPI/ItemStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maen.ItemStoreAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
